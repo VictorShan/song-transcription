6c096d9aac98f7e9832142980923721559d3099d
Victor Shan <victorshanvs@gmail.com>
2023-12-02 16:39:24 -0800

    bad vad cuts

--- data/test.py
+++ data/test.py
@@ -6,6 +6,7 @@ class UtilsTestCase(unittest.TestCase):
     OUTPUT_DIR = Path("data/test_output")
     # https://ccmixter.org/files/admiralbob77/65751
     SAMPLE_MP3 = Path("data/admiralbob77_-_Creator_of_the_Stars_At_Night_3.mp3")
+    vocals_path = None
 
     @classmethod
     def setUpClass(cls):
@@ -13,8 +14,12 @@ class UtilsTestCase(unittest.TestCase):
 
     @classmethod
     def tearDownClass(cls):
-        cls.OUTPUT_DIR.rmdir()
+        if False and cls.vocals_path and cls.vocals_path.exists():
+            cls.vocals_path.unlink()
+        if False and cls.OUTPUT_DIR.exists():
+            cls.OUTPUT_DIR.rmdir()
 
+    # @unittest.skip("Skip testDemucs")
     def testDemucs(self):
         separate_vocals(
             filepath=[self.SAMPLE_MP3],
@@ -29,9 +34,22 @@ class UtilsTestCase(unittest.TestCase):
         no_vocals_path = Path(f"{output_dir}/no_vocals.wav")
         self.assertTrue(vocals_path.exists())
         self.assertTrue(no_vocals_path.exists())
-        vocals_path.unlink()
         no_vocals_path.unlink()
+        return vocals_path
+
+    def testVadCut(self):
+        vocals_path = self.testDemucs()
+        filename = self.SAMPLE_MP3.stem
+        output_dir = Path(f"{self.OUTPUT_DIR}/cut/{filename}")
+        vad_cuts = vad_cut(
+            audio_filepath=vocals_path,
+            output_dir=self.OUTPUT_DIR,
+            max_duration_sec=10,
+            max_silence_sec=2,
+        )
+        self.assertTrue(output_dir.exists())
         output_dir.rmdir()
+        output_dir.parent.rmdir()
 
 
 if __name__ == "__main__":
--- data/utils.py
+++ data/utils.py
@@ -1,5 +1,10 @@
 from pathlib import Path
+from pyannote.audio import Pipeline
+from .env import HF_TOKEN
+from dataclasses import dataclass
 import demucs.separate
+import os
+import torch
 
 def separate_vocals(
         filepath: list[Path],
@@ -24,3 +29,53 @@ def separate_vocals(
         " ".join(f'{str(path)}' for path in filepath),
     ]
     demucs.separate.main(arguments)
+
+@dataclass
+class VadCut:
+    filepath: Path
+    dataset: str
+    duration_ms: int
+
+@dataclass
+class TimedAnnotation:
+    start: int
+    end: int
+    label: str
+
+    def duration(self) -> int:
+        return self.end - self.start
+
+def vad_cut(audio_filepath: Path, output_dir: Path, max_duration_sec: float, max_silence_sec: float) -> list[VadCut]:
+    pipeline = Pipeline.from_pretrained(
+        "pyannote/speaker-diarization-3.1",
+        use_auth_token=HF_TOKEN,
+    )
+    pipeline.to(torch.device("cuda" if torch.cuda.is_available() else "cpu"))
+    print("========== Audio File", audio_filepath)
+    diarization = pipeline(audio_filepath)
+    valid_segements = []
+    for segment, label in diarization.itertracks(yield_label=False):
+        if len(valid_segements) == 0:
+            valid_segements.append(TimedAnnotation(segment.start, segment.end, label))
+        else:
+            last_segment = valid_segements[-1]
+            segment_duration_sec = segment.end - segment.start
+            if last_segment.duration() + segment_duration_sec < max_duration_sec \
+                    and segment.start - last_segment.end < max_silence_sec:
+                last_segment.end = segment.end
+            else:
+                valid_segements.append(TimedAnnotation(segment.start, segment.end, label))
+
+    valid_cuts = []
+    for segment in valid_segements:
+        segment_filepath = Path(f"{output_dir}/{audio_filepath.stem}_{segment.start}_{segment.end}.wav")
+        segment_filepath.parent.mkdir(parents=True, exist_ok=True)
+        os.system(f"ffmpeg -i {audio_filepath} -ss {segment.start} -to {segment.end} -c copy {segment_filepath}")
+        valid_cuts.append(VadCut(segment_filepath, segment.label, segment.duration()*1000))
+    print(f"Valid cuts: {len(valid_cuts)}")
+    return valid_cuts
+
+
+
+
+
